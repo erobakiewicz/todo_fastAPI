ea2718b9fcedfc0f9f76f49cb8e490f5a0344215
erobakiewicz <eliasz.robakiewicz@laboratorium.ee>
2022-02-05 22:48:45 +0100

    create todo endpoint and flake8

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,7 +1,10 @@
+from typing import Optional
+
 from fastapi import FastAPI, Depends, HTTPException
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -16,6 +19,13 @@ def get_db():
         db.close()
 
 
+class Todo(BaseModel):
+    title: str
+    description: Optional[str]
+    priority: int = Field(gt=0, lt=6, description="Priority have to between 1 and 5")
+    complete: bool
+
+
 @app.get("/")
 async def read_all(db: Session = Depends(get_db)):
     return db.query(models.Todos).all()
@@ -27,6 +37,20 @@ async def read_todo(todo_id: int, db: Session = Depends(get_db)):
     if todo_model:
         return todo_model
     raise http_exception()
+
+
+@app.post("/")
+async def create_todo(todo: Todo, db: Session = Depends(get_db)):
+    todo_model = models.Todos()
+    todo_model.title = todo.title
+    todo_model.description = todo.description
+    todo_model.priority = todo.priority
+    todo_model.complete = todo.complete
+
+    db.add(todo_model)
+    db.commit()
+
+    return {'status': 201, 'transaction': 'Successful'}
 
 
 def http_exception():

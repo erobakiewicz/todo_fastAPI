f97484fc24c948dea2c4afc06dc02abc4f2be6b9
erobakiewicz <eliasz.robakiewicz@laboratorium.ee>
2022-02-06 13:37:08 +0100

    added functionality to save users to db

--- TodoApp/auth.py
+++ TodoApp/auth.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 from typing import Optional
 import models
 from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from database import SessionLocal, engine
 
 
 class CreateUser(BaseModel):
@@ -15,7 +17,17 @@ class CreateUser(BaseModel):
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
 
 
 def get_password_hash(password):
@@ -23,7 +35,7 @@ def get_password_hash(password):
 
 
 @app.post("/create/user")
-async def create_new_user(create_user: CreateUser):
+async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)):
     create_user_model = models.Users()
     create_user_model.email = create_user.email
     create_user_model.username = create_user.username
@@ -32,4 +44,5 @@ async def create_new_user(create_user: CreateUser):
     create_user_model.hashed_password = get_password_hash(create_user.password)
     create_user_model.is_active = True
 
-    return create_user_model
+    db.add(create_user_model)
+    db.commit()

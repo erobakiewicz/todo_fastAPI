b840b032c072ced0403004b7ce9a36227254ce44
erobakiewicz <eliasz.robakiewicz@laboratorium.ee>
2022-02-11 19:25:37 +0100

    all the routing and companies app, auth and todos apps integrated via routers to main app

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI, Depends, HTTPException, status
+import sys
+
+sys.path.append("..")
+
+from fastapi import Depends, HTTPException, status, APIRouter
 from pydantic import BaseModel
 from typing import Optional
 import models
@@ -27,7 +31,7 @@ models.Base.metadata.create_all(bind=engine)
 
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl='token')
 
-app = FastAPI()
+router = APIRouter(prefix="/auth", tags=['auth'], responses={401: {"user": "Not authorized"}})
 
 
 def get_db():
@@ -77,7 +81,7 @@ async def get_current_user(token: str = Depends(oauth2_bearer)):
         raise get_user_exception()
 
 
-@app.post("/create/user")
+@router.post("/create/user")
 async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)):
     create_user_model = models.Users()
     create_user_model.email = create_user.email
@@ -91,7 +95,7 @@ async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)
     db.commit()
 
 
-@app.post("/token")
+@router.post("/token")
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:

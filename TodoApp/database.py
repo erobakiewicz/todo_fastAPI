c9b0bf86abafb3ceb39e8c95c69a654ce17e9898
erobakiewicz <eliasz.robakiewicz@laboratorium.ee>
2022-04-01 19:31:09 +0200

    db url

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,9 +1,11 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://apoloniusz:Mysql74267!@127.0.0.1:3306/todo1'
 
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
